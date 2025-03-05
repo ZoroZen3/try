24fb75492d7f3cab5af557e18afe5d2b3fd329bc
saeed <saeedhasanansari@gmail.com>
2025-03-05 11:24:13 +0530

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace MyApplication
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
